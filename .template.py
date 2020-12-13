f70b0967df2b61602d5e12be741008a9eedfa835
Samuel P. Gillispie II <spgillispie2@gmail.com>
2020-12-12 21:05:40 -0500

    Simple template for a solution

---
+++ .template.py
@@ -0,0 +1,20 @@
+# stdlib imports
+import pathlib
+import sys
+
+# vendor imports
+import colorama
+from colorama import Fore, Back, Style
+
+# Initialize colorama
+colorama.init()
+
+# Open input file
+if len(sys.argv) < 2:
+    print(f"{Fore.RED}No input file given{Style.RESET_ALL}")
+    exit()
+inputPath = pathlib.Path(sys.argv[1])
+if not inputPath.exists():
+    print(f"{Fore.RED}Input file does not exist{Style.RESET_ALL}")
+    exit()
+inputHandle = inputPath.open("r")
